1423bc963739f68bb558f0a8bd23d359544b2bd5
Amir Hossain <mr.amir.1971@gmail.com>
2019-02-06 00:40:51 +0600

    room database test done

---
+++ app/build/generated/source/apt/debug/com/example/amir/examtestapp/AppDatabase_Impl.java
@@ -0,0 +1,121 @@
+package com.example.amir.examtestapp;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.db.SupportSQLiteOpenHelper.Callback;
+import android.arch.persistence.db.SupportSQLiteOpenHelper.Configuration;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.RoomOpenHelper;
+import android.arch.persistence.room.RoomOpenHelper.Delegate;
+import android.arch.persistence.room.util.TableInfo;
+import android.arch.persistence.room.util.TableInfo.Column;
+import android.arch.persistence.room.util.TableInfo.ForeignKey;
+import android.arch.persistence.room.util.TableInfo.Index;
+import java.lang.IllegalStateException;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.HashMap;
+import java.util.HashSet;
+
+@SuppressWarnings("unchecked")
+public class AppDatabase_Impl extends AppDatabase {
+  private volatile UserDao _userDao;
+
+  @Override
+  protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration configuration) {
+    final SupportSQLiteOpenHelper.Callback _openCallback = new RoomOpenHelper(configuration, new RoomOpenHelper.Delegate(1) {
+      @Override
+      public void createAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("CREATE TABLE IF NOT EXISTS `User` (`uid` INTEGER NOT NULL, `first_name` TEXT, `last_name` TEXT, PRIMARY KEY(`uid`))");
+        _db.execSQL("CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)");
+        _db.execSQL("INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, \"30356705f25af0455433fcd88a5dc1a9\")");
+      }
+
+      @Override
+      public void dropAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("DROP TABLE IF EXISTS `User`");
+      }
+
+      @Override
+      protected void onCreate(SupportSQLiteDatabase _db) {
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onCreate(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onOpen(SupportSQLiteDatabase _db) {
+        mDatabase = _db;
+        internalInitInvalidationTracker(_db);
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onOpen(_db);
+          }
+        }
+      }
+
+      @Override
+      protected void validateMigration(SupportSQLiteDatabase _db) {
+        final HashMap<String, TableInfo.Column> _columnsUser = new HashMap<String, TableInfo.Column>(3);
+        _columnsUser.put("uid", new TableInfo.Column("uid", "INTEGER", true, 1));
+        _columnsUser.put("first_name", new TableInfo.Column("first_name", "TEXT", false, 0));
+        _columnsUser.put("last_name", new TableInfo.Column("last_name", "TEXT", false, 0));
+        final HashSet<TableInfo.ForeignKey> _foreignKeysUser = new HashSet<TableInfo.ForeignKey>(0);
+        final HashSet<TableInfo.Index> _indicesUser = new HashSet<TableInfo.Index>(0);
+        final TableInfo _infoUser = new TableInfo("User", _columnsUser, _foreignKeysUser, _indicesUser);
+        final TableInfo _existingUser = TableInfo.read(_db, "User");
+        if (! _infoUser.equals(_existingUser)) {
+          throw new IllegalStateException("Migration didn't properly handle User(com.example.amir.examtestapp.User).\n"
+                  + " Expected:\n" + _infoUser + "\n"
+                  + " Found:\n" + _existingUser);
+        }
+      }
+    }, "30356705f25af0455433fcd88a5dc1a9", "64a1bd9c7d4ff1f33c05e4460d6f61c2");
+    final SupportSQLiteOpenHelper.Configuration _sqliteConfig = SupportSQLiteOpenHelper.Configuration.builder(configuration.context)
+        .name(configuration.name)
+        .callback(_openCallback)
+        .build();
+    final SupportSQLiteOpenHelper _helper = configuration.sqliteOpenHelperFactory.create(_sqliteConfig);
+    return _helper;
+  }
+
+  @Override
+  protected InvalidationTracker createInvalidationTracker() {
+    return new InvalidationTracker(this, "User");
+  }
+
+  @Override
+  public void clearAllTables() {
+    super.assertNotMainThread();
+    final SupportSQLiteDatabase _db = super.getOpenHelper().getWritableDatabase();
+    try {
+      super.beginTransaction();
+      _db.execSQL("DELETE FROM `User`");
+      super.setTransactionSuccessful();
+    } finally {
+      super.endTransaction();
+      _db.query("PRAGMA wal_checkpoint(FULL)").close();
+      if (!_db.inTransaction()) {
+        _db.execSQL("VACUUM");
+      }
+    }
+  }
+
+  @Override
+  public UserDao userDao() {
+    if (_userDao != null) {
+      return _userDao;
+    } else {
+      synchronized(this) {
+        if(_userDao == null) {
+          _userDao = new UserDao_Impl(this);
+        }
+        return _userDao;
+      }
+    }
+  }
+}
--- app/src/main/java/com/example/amir/examtestapp/MainActivity.java
+++ app/src/main/java/com/example/amir/examtestapp/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.amir.examtestapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.persistence.room.Room;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<User> userList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").build();
+
+        insert(db);
+
+        fetch(db);
+
+        Handler handler=new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                Toast.makeText(MainActivity.this,msg.getData().getString("data"),Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        new DisplayClass(handler).start();
+
+
+    }
+
+    private void fetch(final AppDatabase db) {
+        Thread fetchThread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+               userList =db.userDao().getAll();
+            }
+        });
+
+        fetchThread.start();
+        try {
+            fetchThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insert(final AppDatabase db){
+
+        Thread insertThread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<User> users=new ArrayList<>();
+
+                for(int i=0;i<4;i++){
+                    User user=new User();
+                    user.uid=i+1;
+                    users.add(user);
+                }
+                db.userDao().insertAll(users.toArray(new User[users.size()]));
+            }
+        });
+
+        insertThread.start();
+        try {
+            insertThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class DisplayClass extends Thread implements Runnable{
+        private Handler handler;
+
+        public DisplayClass(Handler handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public void run() {
+            for (int i=0;i<4;i++){
+                Message message=Message.obtain();
+                Bundle bundle =new Bundle();
+                bundle.putString("data",userList.get(i).uid+"");
+                message.setData(bundle);
+                message.setData(bundle);
+                handler.sendMessage(message);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }

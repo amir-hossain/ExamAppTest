a994c4dd2ddf64c1c6041dc5682f3bfd4ba05bf6
Amir Hossain <mr.amir.1971@gmail.com>
2019-02-18 13:11:39 +0600

    databinding test done

--- app/src/main/java/com/example/amir/examtestapp/MainActivity.java
+++ app/src/main/java/com/example/amir/examtestapp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.amir.examtestapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.amir.examtestapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        User user=new User();
+        user.setName("Amir Hossain");
+        binding.setUser(user);
     }
 }
